0fc1fa013eda307a5698b6978ddd98304a07a71b
gommosvn <gommosvn@d997549a-3ad4-0310-b91a-fb2166dfab67>
2004-03-09 06:04:25 +0000

    Fixed addressing of tss descriptor
    git-svn-id: http://gommo.homelinux.net/svn/repos/os/trunk@23 d997549a-3ad4-0310-b91a-fb2166dfab67

--- kernel/taskm.c
+++ kernel/taskm.c
@@ -13,14 +13,16 @@
 **************************************************************************/
 #include <os/taskm.h>
 
-extern struct tss_descriptor* tss_desc;
+extern struct tss_descriptor tss_desc;
 
 void init_taskm(void)
 {
     //We have to set up a Task Descriptor in the GDT
-    tss_desc->base_address_15_00 = 0xFFFF;
-    tss_desc->base_address_23_16 = 0xFF;
-    tss_desc->base_address_31_42 = 0xFF;
-
-    klprintf(18, "Wrote to tss_desc");
+    tss_desc.base_address_15_00 = 0xFFFF;
+    tss_desc.base_address_23_16 = 0xFF;
+    tss_desc.base_address_31_42 = 0xFF;
+    tss_desc.avl = 0x0;
+    tss_desc.zero1 = 0x0;
+    tss_desc.zero2 = 0x0;
+    tss_desc.dpl = 0x0;
 }
